93ffbcb46d99d38d46f75a376dc0b286f9adcd92
AMA Mazing <alexmalone489@gmail.com>
2023-05-03 01:56:45 +1000

    Update [slug].tsx

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -19,22 +19,9 @@ export default function PostPage({
       {/* Sets the metadata and title of page to post details */}
       <Head>
           <title>{title}</title>
-          <meta property="og:title" content={title}/>
           <meta name="description" content={description} />
-          <meta property="og:description" content={description} />
           <meta property="og:image" content={coverimage} />
           <meta property="og:url" content="amamazing-demo-blog.vercel.app/"/>
-          
-
-          {/* Twitter metadata */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:account_id" content="767377889004093441" /> {/* https://tweeterid.com/ */}
-          <meta name="twitter:creator" content="@ama_mazing_" />
-          <meta name="twitter:site" content="@ama_mazing_"/>
-
-
-          
-
           <link rel="icon" href="\favicon.ico" />
       </Head>
 
